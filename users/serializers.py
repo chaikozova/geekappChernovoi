0124a2dc461e7a530f867eb2f4ca296c0a1f766e
Kirienko Eugene <wtff6a@gmail.com>
2021-03-15 12:19:58 +0600

    working version

---
+++ users/serializers.py
@@ -0,0 +1,75 @@
+from django.contrib.auth import authenticate
+from .models import User
+from rest_framework import serializers
+
+
+class LoginSerializer(serializers.Serializer):
+    email = serializers.CharField(label="email", write_only=True)
+    password = serializers.CharField(label='password',
+                                     style={'input_type': 'password'},
+                                     write_only=True,
+                                     trim_whitespace=False)
+    token = serializers.CharField(label='token', read_only=True)
+
+    def validate(self, attrs):
+        email = attrs.get('email')
+        password = attrs.get('password')
+
+        if email and password:
+            user = authenticate(request=self.context.get('request'), email=email, password=password)
+
+            if not user:
+                msg = 'Unable to log in with provided credentials.'
+                raise serializers.ValidationError(msg, code='authorization')
+        else:
+            msg = 'Must include "username" and "password".'
+            raise serializers.ValidationError(msg, code='authorization')
+
+        attrs['user'] = user
+        return attrs
+
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(style={'input_type': 'password'}, write_only=True, required=True)
+    password2 = serializers.CharField(style={'input_type': 'password'}, write_only=True, required=True)
+
+    class Meta:
+        model = User
+        fields = ['email',
+                  'password',
+                  'password2']
+        extra_kwargs = {
+            'password': {'write_only': True},
+        }
+
+    def save(self):
+        user = User(
+            email=self.validated_data['email'],
+            is_active=True,
+        )
+        password = self.validated_data['password']
+        password2 = self.validated_data['password2']
+
+        if password != password2:
+            raise serializers.ValidationError({'password': 'Passwords must match'})
+        user.set_password(password)
+        user.save()
+        return user
+
+
+class UserListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'email', 'username', 'first_name', 'last_name',
+                  'phone_number', 'telegram', 'instagram', 'github',
+                  'is_staff')
+        read_only_fields = ('created', )
+
+
+class UserRetrieveUpdateDeleteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'email', 'username', 'first_name', 'last_name',
+                  'phone_number', 'telegram', 'instagram', 'github',
+                  'image')
+        read_only_fields = ('created', )
